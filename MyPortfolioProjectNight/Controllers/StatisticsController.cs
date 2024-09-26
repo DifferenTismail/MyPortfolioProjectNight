4f39569197f468efd6853208478a8b5f8a04e477
İsmail Baran <ismailbaran04@gmail.com>
2024-09-26 22:01:58 +0300

    istatistik controller oluşturuldu

---
+++ MyPortfolioProjectNight/Controllers/StatisticsController.cs
@@ -0,0 +1,18 @@
+﻿using MyPortfolioProjectNight.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPortfolioProjectNight.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        DbMyPortfolioNightEntities context = new DbMyPortfolioNightEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
