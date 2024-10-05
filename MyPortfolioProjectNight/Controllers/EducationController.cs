976220ee9e717bf3c5f99704c77cb764582683b8
İsmail Baran <ismailbaran04@gmail.com>
2024-10-05 12:32:53 +0300

    tamamlandı

--- MyPortfolioProjectNight/Controllers/EducationController.cs
+++ MyPortfolioProjectNight/Controllers/EducationController.cs
@@ -1,21 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
 using System.Web;
 using System.Web.Mvc;
-
+using MyPortfolioProjectNight.Models;
 namespace MyPortfolioProjectNight.Controllers
 {
     public class EducationController : Controller
     {
-        // GET: Education
+        DbMyPortfolioNightEntities context = new DbMyPortfolioNightEntities();
         public ActionResult EducationList()
         {
-            return View();
+            var values = context.Education.ToList();
+            return View(values);
         }
+        [HttpGet]
         public ActionResult CreateEducation()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult CreateEducation(Education education)
+        {
+            context.Education.Add(education);
+            context.SaveChanges();
+            return RedirectToAction("EducationList");
+        }
+        public ActionResult DeleteEducation(int id)
+        {
+            var value = context.Education.Find(id);
+            context.Education.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("EducationList");
+        }
+        [HttpGet]
+        public ActionResult UpdateEducation(int id)
+        {
+            var value = context.Education.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public ActionResult UpdateEducation(Education education)
+        {
+            var value = context.Education.Find(education.EducationID);
+            value.Title = education.Title;
+            value.Description = education.Description;
+            value.SubTitle = education.SubTitle;
+            value.StartDate = education.StartDate;
+            value.EndDate = education.EndDate;
+            context.SaveChanges();
+            return RedirectToAction("EducationList");
         }
     }
 }
