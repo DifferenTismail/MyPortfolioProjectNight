1b174884a08ef4e38b3dda3916a1bb64825a5518
İsmail Baran <ismailbaran04@gmail.com>
2024-10-05 11:13:36 +0300

    default controller tamamlandı

--- MyPortfolioProjectNight/Controllers/DefaultController.cs
+++ MyPortfolioProjectNight/Controllers/DefaultController.cs
@@ -13,9 +13,25 @@ namespace MyPortfolioProjectNight.Controllers
         DbMyPortfolioNightEntities context = new DbMyPortfolioNightEntities();
         public ActionResult Index()
         {
+            List<SelectListItem> values = (from x in context.Category.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.CategoryName,
+                                               Value = x.CategoryID.ToString()
+                                           }).ToList();
+            ViewBag.v = values;
             return View();
         }
+        [HttpPost]
+        public ActionResult Index(Contact contact)
+        {
+            contact.SendDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            contact.IsRead = false;
 
+            context.Contact.Add(contact);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
         public PartialViewResult PartialHead()
         {
             return PartialView();
@@ -52,6 +68,35 @@ namespace MyPortfolioProjectNight.Controllers
         public PartialViewResult PartialSkill()
         {
             var values = context.Skill.Where(x => x.Status == true).ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult PartialFooter()
+        {
+            var values = context.SocialMedia.Where(x => x.Active == true).ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult PartialEducation()
+        {
+            var values = context.Education.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult PartialIntern()
+        {
+            var values = context.Intern.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult PartialService()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialPortfolio()
+        {
+            var values = context.Portfolio.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult PartialTestimonial()
+        {
+            var values = context.Testimonial.ToList();
             return PartialView(values);
         }
     }
